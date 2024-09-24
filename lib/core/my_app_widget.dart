6ae949e34fb17dfabd680cdea6326c2aea507f00
Julio Cons <julio.cons@geekbears.com>
2024-09-24 15:20:45 -0700

    configure localizations in the project

--- lib/core/my_app_widget.dart
+++ lib/core/my_app_widget.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_template/generated/l10n.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      title: 'Flutter Template',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text("Hello world"),
+      localizationsDelegates: const [
+        AppLocalizations.delegate, // Add this line
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("es"),
+        Locale("en"),
+      ],
     );
   }
 }
